18dfc44f03534780f741158e87fa21c891d67a1b
Luiz Victor <luizvictor1231@gmail.com>
2022-01-04 13:33:15 -0400

    Ajustes de Responsividade e Menu

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const btn_menu = document.getElementById('btn-menu')
 
+function toggleMenu(event) {
+  if (event.type === 'touchstart') {
+    event.preventDefault()
+  }
+
+  const nav = document.getElementById('nav')
+  nav.classList.toggle('show')
+}
+
+btn_menu.addEventListener('click', toggleMenu)
+
+window.addEventListener('click', () => {
+  nav.classList.remove('show')
+})
+
+btn_menu.addEventListener('click', event => {
+  event.stopPropagation()
+})
